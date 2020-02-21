b6f841f869fc12d3c0328a0b6672c7f1838cc5fb
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-02-21 19:20:17 +0530

    until problem 5

---
+++ OOP With C++/chapter 4/examples/Programming Exercise/5.use of macros/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#define MAX(a,b) ((a>b) ? a : b)
+
+using namespace std;
+
+int main(){
+
+    int i =10,j=9,k=4;
+
+    cout<<"Maximum value is : "<<MAX((i,j),k);
+
+}
+
+
+
