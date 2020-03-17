baaca7b1e172dc2ed3f5ba8c2ced77d6ad4256d4
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-03-17 18:12:41 +0530

    programming exercise start

--- OOP With C++/chapter 5/programming exercises/5.1 create class to represent the bank account/main.cpp
+++ OOP With C++/chapter 5/programming exercises/5.1 create class to represent the bank account/main.cpp
@@ -6,6 +6,14 @@ using namespace std;
 class item{
     string name_depositors[20];
     int account_number;
+    int type_of_account;
+    float balance;
+
+public :
+    void initialvalue();
+    void depositea_amount(float);
+    void withdraw_check(float);
+    void display();
     };
 
 int main(){
