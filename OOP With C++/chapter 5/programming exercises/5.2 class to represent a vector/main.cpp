bb2f9c18cb56b0fdd36d9a630fe417d17f987db1
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-03-19 17:19:43 +0530

    programming exercise 5.2

--- OOP With C++/chapter 5/programming exercises/5.2 class to represent a vector/main.cpp
+++ OOP With C++/chapter 5/programming exercises/5.2 class to represent a vector/main.cpp
@@ -1,5 +1,50 @@
 #include<iostream>
 using namespace std;
 
+class vectors{
+    float *vectorarray;
+    int elements;
+
+public :
+    int vectorfun(){
+        cout << "*********************************************************" << endl;
+        cout << "Function to Add vectors : \n\n";
+        cout << "Enter the number of float values you want to enter : " ;
+        cin >> elements ;
+
+            vectorarray = new float[elements];
+                cout << "\n Enter Your Float values One by one : " << endl ;
+
+            for(int i=0;i<elements;i++){
+            cout << i+1<<" float value : " ; cin >> vectorarray[i] ;
+            }
+    cout << "*********************************************************" << endl;
+    }
+
+    int scalar_multiply(){
+        cout << "*********************************************************" << endl;
+        cout << "Function to Multiply scalar : \n\n";
+    }
+
+    int vectordisplay(){
+        cout << "*********************************************************" << endl;
+
+        cout << "Function to Display vectors : \n\n";
+
+        cout << "\n Elements of vectors : " << endl ;
+         for(int i=0;i<elements;i++){
+            cout << i+1<<" float value : " << vectorarray[i] << endl;
+            }
+    cout << "*********************************************************" << endl;
+    }
+
+};
+
 int main(){
-}
+    vectors varray;
+    varray.vectorfun();
+    varray.vectordisplay();
+
+    return 0;
+
+    }
