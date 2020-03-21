b39f365c88fb0452aab667ed9e8d8a1db57f1fcf
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-03-21 14:23:44 +0530

    programming exercise 5.5 start

--- OOP With C++/chapter 5/programming exercises/5.5/main.cpp
+++ OOP With C++/chapter 5/programming exercises/5.5/main.cpp
@@ -5,24 +5,5 @@ using namespace std;
 
 int main(){
 
-                cout << "\n 6.Quite\n";
-                cout << "\n What is your Option : ";
-
-                cin >> choise ;
-                switch(choise){
-                    case 1 : varray.vectorfun();break;
-                    case 2 : varray.modify_vector();break;
-                    case 3 : varray.scalar_multiply();break;
-                    case 4 : varray.addvectors();break;
-                    case 5 : varray.vectordisplay();break;
-                    case 6 : break;
-                    default : "choice is invalid ; select appropriate number !";
-                }
-    }
-
-
-
-
-    return 0;
 
     }
