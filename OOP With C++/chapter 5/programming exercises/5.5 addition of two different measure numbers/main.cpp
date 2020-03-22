94dbd3e812a348925f9b9711ab740a81d7d935d8
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-03-22 19:23:04 +0530

    till 5.5

---
+++ OOP With C++/chapter 5/programming exercises/5.5 addition of two different measure numbers/main.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+class DB;
+class DM{
+    float metres;
+    float centimeters;
+public:
+    void get_data(int m,int c){
+        metres = m;
+        centimeters = c;
+    }
+    void put_data(){
+        cout << metres <<" Meters and "<< centimeters << " centimeters." << endl;
+    }
+
+    friend DM addition(DM,DB);
+};
+
+class DB{
+    float feet;
+    float inches;
+public:
+    void get_data(int f,int i){
+        feet = f;
+        inches = i;
+    }
+    void put_data(){
+        cout << feet <<" feet and "<< inches << " inches." << endl;
+    }
+
+    friend DM addition(DM,DB);
+};
+
+DM addition(DM mc, DB fi){
+    mc.metres += fi.feet*0.3048;
+    mc.centimeters += fi.inches*2.54;
+
+    return mc;
+}
+
+int main(){
+
+    DM ometric;
+    DB oenglish;
+
+    ometric.get_data(5,50);
+    oenglish.get_data(5,50);
+
+    cout << " Dimensions in Metric : " ; ometric.put_data();
+
+    cout << " Dimensions in English : " ; oenglish.put_data();
+
+    DM oaddition;
+
+    oaddition = addition(ometric,oenglish);
+
+    cout << " Dimensions in Metric (After addition) : " ; oaddition.put_data();
+
+    return 0;
+    }
--- OOP With C++/chapter 5/programming exercises/5.5/main.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
-using namespace std;
-
-int main(){
-
-
-    }
