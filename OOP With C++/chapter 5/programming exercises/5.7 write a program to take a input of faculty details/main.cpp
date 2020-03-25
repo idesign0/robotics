1c785e74aa2da3f9838a3458de1a8e16b2bf1f01
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-03-25 12:51:33 +0530

    5.7 start

--- OOP With C++/chapter 5/programming exercises/5.7 write a program to take a input of faculty details/main.cpp
+++ OOP With C++/chapter 5/programming exercises/5.7 write a program to take a input of faculty details/main.cpp
@@ -12,15 +12,19 @@ class faculty{
     char address[35];
 
 public :
-    void get_data(){
+    void get_data();
+    void put_data();
+};
+
+    void faculty::get_data(){
         cout << "****************************************************************\n";
         cout << "Enter Asked Details below : \n\n";
-        cout << "Enter faculties id : ";
+        cout << "Enter faculty's id : ";
         cin >> id ;
-        cout << "Enter faculties full Name (surname name father's name): ";
+        cout << "Enter faculty's full Name (surname name father's name): ";
         cin.get(name,30);
 
-        cout << "Enter faculties Post: \n";
+        cout << "Enter faculty's Post: \n";
         cout << "\t Instructions : \n";
         cout << "\t Press 1 for post of instructor : \n";
         cout << "\t Press 2 for post of Assistant_Professor : \n";
@@ -29,14 +33,37 @@ public :
         cout << "\t Enter your post : ";
         cin >> index;
 
-        cout << "Enter faculties qualification : \n";
+        cout << "Enter faculty's qualification : \n";
         cin.get(qualification,30);
 
-        cout << "Enter faculties address : \n";
+        cout << "Enter faculty's address : \n";
         cin.get(address,35);
+        cout << "****************************************************************\n";
+        }
+
+    void faculty::put_data(){
+
+        cout << "****************************************************************\n";
+        cout << "faculty's ID : " << id <<endl;
+        cout << "faculty's full Name (surname name father's name): " << name[30] << endl;
+
+        cout << "Enter faculty's Post: ";
+        index -= index;
+        switch(index){
+            case Instructor : cout << "Instructor" <<endl; break;
+            case Assistant_Professor : cout << "Assistant Professor" <<endl; break;
+            case Associate_Professor : cout << "Associate Professor" <<endl; break;
+            case Professor : cout << "Professor" <<endl; break;
+            default : cout << "Invalid information" << endl;break;
 
         }
-};
+
+        cout << "faculty's qualification : " << qualification[30];
+
+        cout << "Enter faculty's address : " << address[35];
+        cout << "****************************************************************\n";
+    }
+
 
 int main(){
 
