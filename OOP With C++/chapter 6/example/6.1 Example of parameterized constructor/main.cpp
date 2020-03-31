581c9ed6482b7bd52b1c3238cba7afe3711c88cc
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-03-31 15:28:35 +0530

    starting 5.8

--- OOP With C++/chapter 6/example/6.1 Example of parameterized constructor/main.cpp
+++ OOP With C++/chapter 6/example/6.1 Example of parameterized constructor/main.cpp
@@ -1,21 +1,27 @@
 #include<iostream>
 using namespace std;
 
-class test{
-    int x;
+class point{
+    int x,y;
 public:
-    test(int y =0){
-        x=y;
-        cout << x;
-    }
-    test(){
-        x=0;
-        cout << x;
+    point(int a,int b){
+        x=a;
+        y=b;
+        }
+    void display(){
+        cout << "("<<x<<","<<y<<")\n";
     }
 };
 
 int main(){
-    test t;
-    test t1(4);
+    point p1(3,4);
+    point p2(5,5);
+
+    cout << "Point p1 = ";
+    p1.display();
+
+    cout << "Point p2 = ";
+    p2.display();
+    return 0;
 return 0;
 }
