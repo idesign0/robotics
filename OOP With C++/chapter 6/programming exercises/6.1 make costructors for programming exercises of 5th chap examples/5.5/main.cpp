fccc474075f190d9bd79a524905a84aac5c70e2f
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-02 15:21:49 +0530

    5.5

---
+++ OOP With C++/chapter 6/programming exercises/6.1 make costructors for programming exercises of 5th chap examples/5.5/main.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class test{
+    int code;
+    static int countt;
+public:
+    test(){
+        code = ++countt;
+    }
+    void showcode(){
+        cout << "Object Number : " << code << "\n";
+    }
+
+    static void showcount(){
+        cout << "Count : " << countt << "\n";
+    }
+};
+int test::countt;
+int main(){
+    test t1,t2;
+    test::showcount();
+
+    test t3;
+
+    test::showcount();
+
+    t1.showcode();
+    t2.showcode();
+    t3.showcode();
+    return 0;
+}
