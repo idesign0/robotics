eead7fc9e63c574839163b588a32179ab19cf36a
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-05 14:32:10 +0530

    6.3

--- OOP With C++/chapter 6/programming exercises/6.3 class of book/main.cpp
+++ OOP With C++/chapter 6/programming exercises/6.3 class of book/main.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<iomanip>
-#include<string>
+#include<cstring>
 using namespace std;
 class book{
     char* title;
@@ -11,10 +11,15 @@ class book{
     int stock;
 public:
     book(){}
-    book(string t,string a,string p){
-        title = t;
-        author = a;
-        publisher = p;
+    book(const char* t,const char* a,const char* p){
+
+        title = new char[strlen(t)+1];
+        author = new char[strlen(a)+1];
+        publisher = new char[strlen(p)+1];
+
+        strcpy(title,t);
+        strcpy(author,a);
+        strcpy(publisher,p);
 
         cout << title << endl;
         cout << author << endl;
@@ -33,16 +38,22 @@ int main(){
     cout << "Give your price : ";
     cin >> option;
     cout << "\nEnter book title : ";
-    cin.ignore('\n');
-    cin >> booktitle;
-    cout << "\nEnter book author : ";
-    cin.ignore('\n');
-    cin >> bookauthor;
-    cout << "\nEnter book publisher : ";
     cin.ignore();
-    cin >> bookpublisher;
+    getline(cin,booktitle);
 
-    book(booktitle,bookauthor,bookpublisher);
+    cout << "Enter book author : ";
+    cin.ignore();
+    getline(cin,bookauthor);
+
+    cout << "Enter book publisher : ";
+    cin.ignore();
+    getline(cin,bookpublisher);
+
+    const char* c_booktitle = booktitle.c_str();
+    const char* c_bookauthor = bookauthor.c_str();
+    const char* c_bookpublisher = bookpublisher.c_str();
+    book(c_booktitle,c_bookauthor,c_bookpublisher);
+
     }while(option!=0);
     return 0;
 }
