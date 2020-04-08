c9cf34f450c9c465b44f8b6aec2affec7ea39c01
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-08 15:44:45 +0530

    6.3

--- OOP With C++/chapter 6/programming exercises/6.3 class of book/main2.cpp
+++ OOP With C++/chapter 6/programming exercises/6.3 class of book/main2.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<string.h>
-
+#include<stdlib.h>
 using namespace std;
 
 class book{
@@ -18,25 +18,33 @@ public:
     }
 
     void feeddata();
+    void editdata();
     void showdata();
     void buybook();
     int searchh(char[],char[]);
 };
     void book::feeddata(){
         cin.ignore();
-        cout << "Enter Books name : " ; cin.getline(title,20);
+        cout << "\nEnter Books name : " ; cin.getline(title,20);
         cout << "Enter authors name : " ; cin.getline(author,20);
         cout << "Enter publisher name : " ; cin.getline(publisher,20);
         cout << "Enter Price : "; cin >> *price;
         cout << "Enter Stock position : "; cin >> *stock;
     }
-
+    void book::editdata(){
+        cin.ignore();
+        cout << "\nEnter Books name : " ; cin.getline(title,20);
+        cout << "Enter authors name : " ; cin.getline(author,20);
+        cout << "Enter publisher name : " ; cin.getline(publisher,20);
+        cout << "Enter Price : "; cin >> *price;
+        cout << "Enter Stock position : "; cin >> *stock;
+    }
     void book::showdata(){
-        cout << "Enter Books name : " << title;
-        cout << "Enter authors name : " << author;
-        cout << "Enter publisher name : " << publisher;
-        cout << "Enter Price : "<< *price;
-        cout << "Enter Stock position : "<< *stock;
+        cout << "\nEnter Books name : " << title;
+        cout << "\nEnter authors name : " << author;
+        cout << "\nEnter publisher name : " << publisher;
+        cout << "\nEnter Price : "<< *price;
+        cout << "\nEnter Stock position : "<< *stock;
     }
     void book::buybook(){
         int countt;
@@ -44,12 +52,13 @@ public:
         if(countt<=*stock){
             *stock = *stock - countt;
             cout << "\nBooks bought successfully";
-            cout << "\nAmount : " << (*stock)*price;
+            cout << "\nAmount : " << (*price)*countt<<endl;}
         else{
             cout << "\nRequire copies are not in stock";
         }
+
         }
-    }
+
     int book::searchh(char tbuy[20],char abuy[20]){
         if(strcmp(tbuy,title)==0 && strcmp(abuy,author)==0){
             return 1;
@@ -63,10 +72,11 @@ int main(){
 	int i=0,r,t,choice;
 	char titlebuy[20],authorbuy[20];
 	while(1)	{
-		cout<<"\n\n\t\tMENU"
+		cout<<"\n\nMENU"
 		<<"\n1. Entry of New Book"
 		<<"\n2. Buy Book"
 		<<"\n3. Display Books"
+		<<"\n4. Edit Books data"
 		<<"\n5. Exit"
 		<<"\n\nEnter your Choice: ";
 		cin>>choice;
@@ -77,8 +87,47 @@ int main(){
                 i++; break;
 
         case 2 : cin.ignore();
-        cout << "\nEnter title of the book "; cin.getline(titlebuy,20);
-        cout << "\nEnter name of author of the book "; cin.getline(authorbuy,20);
+        cout << "\nEnter title of the book : "; cin.getline(titlebuy,20);
+        cout << "\nEnter name of author of the book : "; cin.getline(authorbuy,20);
+        for(t=0;t<i;t++){
+            if(B[t]->searchh(titlebuy,authorbuy)){
+                B[t]->buybook();
+                break;
+            }
+            }
+        if(t==i){
+            cout<<"\nThis Book is Not in Stock";
+            break;
+        }
+        case 3 :  cin.ignore();
+				cout<<"\nEnter Title Of Book : "; cin.getline(titlebuy,20);
+				cout<<"Enter Author Of Book : ";  cin.getline(authorbuy,20);
+
+				for(t=0;t<i;t++)	{
+					if(B[t]->searchh(titlebuy,authorbuy))	{
+						cout<<"\nBook Found Successfully";
+						B[t]->showdata();
+						break;
+					}
+				}
+				if(t==i)
+				cout<<"\nThis Book is Not in Stock";
+				break;
+        case 4 : cin.ignore();
+        cout << "Enter Book Title : " ;cin.getline(titlebuy,20);
+        cout << "Enter Book author name : " ;cin.getline(authorbuy,20);
+            for (t=0;t<i;t++){
+                if(B[i]->searchh(titlebuy,authorbuy)){
+                    B[i]->editdata();
+                    break;
+                }
+            }
+            if(t==i)
+                cout<<"\nThis Book is Not in Stock";
+				break;
+        case 5 : exit(0);
+        default: cout<<"\nInvalid Choice Entered";
+
     }
 	}
     return 0;
