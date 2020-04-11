538057bd8b2180784cd3204ed525123e5c7bfb88
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-11 13:33:03 +0530

    6.6 added

--- OOP With C++/chapter 6/programming exercises/6.6 write a program to find prime numbers/main.cpp
+++ OOP With C++/chapter 6/programming exercises/6.6 write a program to find prime numbers/main.cpp
@@ -10,22 +10,24 @@ public:
     void check_number(){
         for(int i=2;i<=value/2;i++){
             if(value%i==0){
-                value=0;
+                k=0;
             }else{
-                value=1;
+                k=1;
             }
         }
     }
     void show_number(){
         cout << "Value : " << value << endl << endl;
         if(k==0){
-            cout << "Value is prime number" << endl;
+            cout << "Value is not prime number" << endl;
         }
         else
-            cout << "Value is not prime number\n";
+            cout << "Value is prime number\n";
     }
 };
 int main(){
-
+        prime o_prime(7);
+        o_prime.check_number();
+        o_prime.show_number();
     return 0;
 }
