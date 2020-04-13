90958b895257b874450608381e78bc65beee0633
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-13 17:00:38 +0530

    6.7

--- OOP With C++/chapter 6/programming exercises/6.7 display car features and specifications/main.cpp
+++ OOP With C++/chapter 6/programming exercises/6.7 display car features and specifications/main.cpp
@@ -1,8 +1,29 @@
 #include<iostream>
+#include<string>
 using namespace std;
 class car{
+    string name;  //name
+    string model; // model
+    string color;  // color
+    int	year;  // year
+    int mileage;  // miles on car
+	string type;  //Type of vehicle
+
 public:
     car(){};
+
+    car(){
+     cin.ignore();
+     cout << "Enter Car name : "; getline(cin,name);
+     cout << "Enter Car model : "; getline(cin,model);
+     cout << "Enter Car color : "; getline(cin,color);
+     cout << "Enter Car year : "; cin>>year;
+     cout << "Enter Car mileage : "; cin>>mileage;
+     cout << "Enter Car type : "; getline(cin,type);
+    }
+
+    void show_details();
+
 };
 int main(){
 
