9a680612d3f3dc16333388f9093f83d3c2c7c40d
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-13 18:15:00 +0530

    6.7

--- OOP With C++/chapter 6/programming exercises/6.7 display car features and specifications/main.cpp
+++ OOP With C++/chapter 6/programming exercises/6.7 display car features and specifications/main.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 #include<string>
+#include<iomanip>
 using namespace std;
+
+int space = 20;
 class car{
     string name;  //name
     string model; // model
@@ -10,23 +13,57 @@ class car{
 	string type;  //Type of vehicle
 
 public:
-    car(){};
-
     car(){
+     name=" ";
+     model=" ";
+     color=" ";
+     year=0;
+     mileage=0;
+     type=" ";
+    }
+    void feed_data();
+    void listof_names();
+    void show_details();
+};
+void car::feed_data(){
      cin.ignore();
      cout << "Enter Car name : "; getline(cin,name);
      cout << "Enter Car model : "; getline(cin,model);
      cout << "Enter Car color : "; getline(cin,color);
+     cout << "Enter Car type : "; getline(cin,type);
+
      cout << "Enter Car year : "; cin>>year;
      cout << "Enter Car mileage : "; cin>>mileage;
-     cout << "Enter Car type : "; getline(cin,type);
-    }
-
-    void show_details();
-
-};
+}
+void car::listof_names(){
+    cout << name << setw(space) << model << endl;
+}
 int main(){
+    int sizee=0,i=0;
+    car *o_car[0];
+    while(1){
+    int option;
+        cout << "\n1. Enter new Car details"
+             << "\n2. List the names of Cars"
+             << "\n3. Show Details of particular Car"
+             << "\n4. Update Details of particular Car"
+             << "\nChoose appropriate option : "; cin>>option;
 
+        switch(option){
+            case 1 :o_car[sizee]= new car;
+                     o_car[sizee]->feed_data();
+                     sizee++;
+                     break;
 
+            case 2 : cout << "Car details : " ;
+                    for(i=0;i<sizee;i++){
+                        o_car[i]->listof_names();
+                    }
+                    if(i==sizee){
+                        cout << "There is no details of car in list";
+                    }break;
+        }
+
+    }
     return 0;
 }
