ddc0fe43efc2b109a2f4d34accfa943d09ec4c0f
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-18 12:44:28 +0530

    chapter 7 starting

--- OOP With C++/chapter 7/examples/Overloading unary minus/main.cpp
+++ OOP With C++/chapter 7/examples/Overloading unary minus/main.cpp
@@ -21,18 +21,21 @@ public:
     }
 
 
-    friend void operator-(space &s){
+    friend space operator-(space &s){
         s.x = -s.x;
         s.y = -s.y;
         s.z = -s.z;
+
+        return s;
     }
 };
 int main(){
     space s(1,2,3);
     cout << "S : \n"; s.display();
 
-    -s;
-    cout << "-S : \n"; s.display();
+    space s1;
+    s1 = -s;
+    cout << "-S : \n"; s1.display();
 
     return 0;
 }
