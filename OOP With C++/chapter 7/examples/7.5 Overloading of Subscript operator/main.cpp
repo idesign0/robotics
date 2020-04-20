823970e8732b5753eeebe938b616e5c2568f513f
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-20 16:41:49 +0530

    7.5

--- OOP With C++/chapter 7/examples/7.5 Overloading of Subscript operator/main.cpp
+++ OOP With C++/chapter 7/examples/7.5 Overloading of Subscript operator/main.cpp
@@ -8,13 +8,17 @@ public:
         for(int i=0;i<5;i++)
             a[i]=x[i];
     }
-    int operator[](int k)[
-        return x[k];
-    ]
+    int operator[](int k){
+        return a[k];
+    }
 };
 int main(){
-    x[5]={1,2,1,3,5};
+    int x[5]={1,2,1,3,5};
 
+    arr A(x);
 
+    for(int i=0;i<5;i++){
+        cout << x[i];
+    }
     return 0;
 }
