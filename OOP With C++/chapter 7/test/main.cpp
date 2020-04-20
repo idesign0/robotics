d0c9c957586cfde14cefbc5475a72abb5f0e086b
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-20 18:03:08 +0530

    7.6

--- OOP With C++/chapter 7/test/main.cpp
+++ OOP With C++/chapter 7/test/main.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
 
 using namespace std;
+class test{
+public:
+    int operator++(int a){
+        a +=5;
+        return a;
+        }
 
 int main(){
-
+    test A;
+    int a = 0;
+    int c=A.operator++(a);
+    cout << c;
 }
