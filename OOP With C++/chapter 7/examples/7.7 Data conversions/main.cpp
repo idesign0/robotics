f47d5339259e2a3a8c4d484079f5e11aebc5b80e
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-21 15:23:43 +0530

    6.7

--- OOP With C++/chapter 7/examples/7.7 Data conversions/main.cpp
+++ OOP With C++/chapter 7/examples/7.7 Data conversions/main.cpp
@@ -32,10 +32,26 @@ class invent_2{
     int code;
     float total_price;
 public:
-
+    invent_2{code=0;total_price=0;}
+    invent_2(int c,int t){
+        code = c; int total_price = t;
+    }
+    void putdata(){
+        cout << "Code : " << code << "\n";
+        cout << "Price : " << total_price << "\n\n";
+    }
+    invent_2(invent_1 v){
+        code = v.get_code();
+        total_price = v.get_items*v.get_price;
+    }
 
 };
 int main(){
+    invent_1 s1(5,500.0,15);
+    invent_2 d1;
+
+    float total_price;
+    total_price = s1;
 
     return 0;
 }
