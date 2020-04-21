f794a58858cb1d723985f7eadf0627e30bbff38d
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-21 15:27:58 +0530

    6.7

--- OOP With C++/chapter 7/examples/7.7 Data conversions/main.cpp
+++ OOP With C++/chapter 7/examples/7.7 Data conversions/main.cpp
@@ -19,9 +19,9 @@ public:
         cout << "Items : " << items << "\n";
         cout << "Price : " << price << "\n\n";
     }
-    int get_code{return code;}
-    float get_price{return price;}
-    int get_items{return items;}
+    int get_code(){return code;}
+    float get_price(){return price;}
+    int get_items(){return items;}
 
     operator float(){
         return (price*items);
@@ -32,7 +32,7 @@ class invent_2{
     int code;
     float total_price;
 public:
-    invent_2{code=0;total_price=0;}
+    invent_2(){code=0;total_price=0;}
     invent_2(int c,int t){
         code = c; int total_price = t;
     }
@@ -42,12 +42,12 @@ public:
     }
     invent_2(invent_1 v){
         code = v.get_code();
-        total_price = v.get_items*v.get_price;
+        total_price = v.get_items() * v.get_price();
     }
 
 };
 int main(){
-    invent_1 s1(5,500.0,15);
+    invent_1 s1(5,500.50,15);
     invent_2 d1;
 
     float total_price;
@@ -56,6 +56,11 @@ int main(){
     cout << "Details of inventory 1 : " << "\n";
     s1.putdata();
     cout << "Total price : " << total_price << "\n\n";
+
+    d1=s1;
+
+    cout << "Details of inventory 2 : " << "\n";
+    d1.putdata();
 
 
     return 0;
