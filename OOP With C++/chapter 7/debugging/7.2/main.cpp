421bcd30efa6b2b897c9631ecc89a87391ac8cc0
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-22 13:09:54 +0530

    7.2

--- OOP With C++/chapter 7/debugging/7.2/main.cpp
+++ OOP With C++/chapter 7/debugging/7.2/main.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+using namespace std;
 
+class weekdays{
+    char* day;
+public:
+
+    weekdays(char * c){
+        day = c;
+    }
+
+bool operator==(weekdays &w2){
+
+    if( day==w2.day)
+        return 1;
+    else
+        return 0;
+}
+};
+
+int main(){
+    weekdays w1 = "sunday", w2="monday";
+    if(w1==w2)
+        cout << "Same day";
+    else
+        cout << "Different day";
+
+}
