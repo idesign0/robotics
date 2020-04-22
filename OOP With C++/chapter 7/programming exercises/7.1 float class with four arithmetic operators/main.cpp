c0f2ce901bc05867bce7cd5d101bc5b754f74fbf
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-22 15:06:31 +0530

    7.3

--- OOP With C++/chapter 7/programming exercises/7.1 float class with four arithmetic operators/main.cpp
+++ OOP With C++/chapter 7/programming exercises/7.1 float class with four arithmetic operators/main.cpp
@@ -4,11 +4,16 @@ using namespace std;
 class Float{
     float var_float;
 public:
+    Float(){}
     Float(float var){var_float=var;}
     Float operator+(Float A);
     Float operator-(Float A);
     Float operator*(Float A);
     Float operator/(Float A);
+
+    void show(){
+    cout << var_float;
+    }
 };
 
 Float Float::operator+(Float A){
@@ -28,5 +33,22 @@ Float Float::operator/(Float A){
 }
 
 int main(){
+    Float a,b,c;
+    a=10;
+    b=2;
+
+    cout << "Sum of two objects : ";
+    c = a + b ; c.show();
+
+    cout << "\n\nSubtraction of two objects : ";
+    c = a - b ; c.show();
+
+    cout << "\n\nMultiplication of two objects : ";
+    c = a * b ; c.show();
+
+    cout << "\n\nDivision of two objects : ";
+    c = a / b ; c.show();
+
+
     return 0;
 }
