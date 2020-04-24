589f0c64682e692fd5e79e255ebb94d83d49fbb8
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-24 20:45:56 +0530

    additional numbers

--- OOP With C++/chapter 7/programming exercises/7.7 compare two factorial numbers/main.cpp
+++ OOP With C++/chapter 7/programming exercises/7.7 compare two factorial numbers/main.cpp
@@ -18,17 +18,57 @@ public:
     void show(){
         cout << "Value : " << value <<" factorial value : " << facto_value << endl;
     }
-
+    int get_factovalue(){return facto_value;}
     friend int operator==(factorial &,factorial &);
+    friend int operator>=(factorial &,factorial &);
+    friend int operator<=(factorial &,factorial &);
+    friend int operator!=(factorial &,factorial &);
+
 };
 
-int operator==(factorial &,factorial &){
-
+int operator==(factorial &a,factorial &b){
+    if(a.get_factovalue()==b.get_factovalue()){
+        return 1;
+    }else
+    return 0;
 }
 
-int main(){
-    factorial Factvalue = 5 ;
+int operator>=(factorial &a,factorial &b){
+    if(a.get_factovalue()>=b.get_factovalue()){
+        return 1;
+    }else
+    return 0;
+}
 
-    Factvalue.show();
+int operator<=(factorial &a,factorial &b){
+    if(a.get_factovalue()<=b.get_factovalue()){
+        return 1;
+    }else
+    return 0;
+}
+
+int operator!=(factorial &a,factorial &b){
+    if(a.get_factovalue()!=b.get_factovalue()){
+        return 1;
+    }else
+    return 0;
+}
+int main(){
+    factorial Factvalue1 = 5 ;
+    factorial Factvalue2 = 10 ;
+
+    cout << "If two values are equal : ";
+    (Factvalue1==Factvalue2) ? cout << "True" << endl : cout << "False" << endl;
+
+    cout << "If 1 is greater than 2 : ";
+    (Factvalue1>=Factvalue2) ? cout << "True" << endl : cout << "False"<< endl;
+
+
+    cout << "If 1 is less than 2 : ";
+    (Factvalue1<=Factvalue2) ? cout << "True" << endl : cout << "False"<< endl;
+
+    cout << "If two values are not equal : ";
+    (Factvalue1!=Factvalue2) ? cout << "True" << endl : cout << "False"<< endl;
+
     return 0;
 }
