cb2990e42999078ff1a65a7067df8194699c8158
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-24 20:34:04 +0530

    additional numbers

--- OOP With C++/chapter 7/programming exercises/7.7 compare two factorial numbers/main.cpp
+++ OOP With C++/chapter 7/programming exercises/7.7 compare two factorial numbers/main.cpp
@@ -1,1 +1,34 @@
+#include<iostream>
+using namespace std;
 
+int fact_fun(int v){
+            if(v==1){
+                return 1;
+            }else{
+                return v * fact_fun(v-1);
+            }
+        }
+
+class factorial{
+    int value;
+    int facto_value=fact_fun(value);
+public:
+    factorial(int v):value(v){
+    }
+    void show(){
+        cout << "Value : " << value <<" factorial value : " << facto_value << endl;
+    }
+
+    friend int operator==(factorial &,factorial &);
+};
+
+int operator==(factorial &,factorial &){
+
+}
+
+int main(){
+    factorial Factvalue = 5 ;
+
+    Factvalue.show();
+    return 0;
+}
