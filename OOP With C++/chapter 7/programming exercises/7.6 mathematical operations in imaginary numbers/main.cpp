e0fef135e53432a133193fe3ff6877aa5ac8d351
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-24 17:42:16 +0530

    additional numbers

--- OOP With C++/chapter 7/programming exercises/7.6 mathematical operations in imaginary numbers/main.cpp
+++ OOP With C++/chapter 7/programming exercises/7.6 mathematical operations in imaginary numbers/main.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+using namespace std;
 
+class complexx{
+    float real;
+    float image;
+
+public:
+    complexx(float r,float i){
+        real=r;
+        image=i;
+    }
+};
+
+int main(){
+
+    return 0;
+}
