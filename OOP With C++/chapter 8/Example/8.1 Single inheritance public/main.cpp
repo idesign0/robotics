d73ff5bde94b3139697fc22f24474b7d7d3b1fd2
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-25 19:18:10 +0530

    single inheritance

--- OOP With C++/chapter 8/Example/8.1 Single inheritance public/main.cpp
+++ OOP With C++/chapter 8/Example/8.1 Single inheritance public/main.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+class b{
+    int a;
+public:
+    int b;// public; ready for inheritance
+    void set_ab();
+    int get_a();
+    void show_a();
+};
 
+int main(){
     return 0;
 }
