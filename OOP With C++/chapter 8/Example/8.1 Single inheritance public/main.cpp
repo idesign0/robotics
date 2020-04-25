b5073c265d60870017b09f671a752ee47a417bda
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-25 19:52:15 +0530

    single inheritance

--- OOP With C++/chapter 8/Example/8.1 Single inheritance public/main.cpp
+++ OOP With C++/chapter 8/Example/8.1 Single inheritance public/main.cpp
@@ -4,21 +4,32 @@ using namespace std;
 class A{
     int a;
 public:
-    int b;
+    int b=10;
 
-    void set_x{a=10;}
-    void get_x{return a;}
-    void show{cout << "X : "<<x << endl;}
+    void set_x(){a=10;}
+    int get_x(){return a;}
+    void show_x(){cout << "X : "<<a << endl;}
 };
 
 class B : public A{
     int c;
 public:
     void mul(){ c = b*get_x();}
-    void show(){cout << "C : " << c << endl;}
+    void show(){cout << "B : " << b << endl;
+                cout << "C : " << c << "\n\n";}
 };
 
 int main(){
-    B b;
+    B d;
+
+    d.show_x();
+    d.mul();
+    d.show();
+
+    d.show_x();
+    d.b=20;
+    d.mul();
+    d.show();
+
     return 0;
 }
