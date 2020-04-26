2777218e6ff90963007996dd03b6c9b978c00b22
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-26 19:20:49 +0530

    multilevel inheritance

--- OOP With C++/chapter 8/test/multilevel ineritance/main.cpp
+++ OOP With C++/chapter 8/test/multilevel ineritance/main.cpp
@@ -1,17 +1,51 @@
 #include<iostream>
 using namespace std;
 
+// class student numbers
+
 class student_number{
     int roll_number;
 public:
     void get_number(int number){ roll_number = number;}
     void put_number(){cout << "Student Number : "<< roll_number << endl;}
 };
-class student_marks{
-    int marks;
-    protected
-};
-int main(){
 
+// class test
+
+class student_test:public student_number{
+protected:
+    float sub1;
+    float sub2;
+public :
+    void get_marks(float a,float b){
+    sub1=a;
+    sub2=b;
+    }
+    void put_marks(){
+        cout << "Marks in SUB1 : " << sub1 <<endl
+             << "Marks in SUB2 : " << sub2 << endl;
+    }
+};
+
+// student total
+
+class total:public student_test{
+    float total;
+public:
+    void display();
+};
+
+void total::display(){
+    total = sub1 + sub2;
+    put_number();
+    put_marks();
+    cout << "Total : "<<total <<"\n";
+}
+
+int main(){
+    total t;
+    t.get_number(111);
+    t.get_marks(11.0,50.51);
+    t.display();
     return 0;
 }
