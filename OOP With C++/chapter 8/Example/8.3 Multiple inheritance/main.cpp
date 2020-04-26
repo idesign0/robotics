91beb45e165a117de8f0f66d8fb93f1208d1fb47
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-26 22:14:11 +0530

    multiple inheritance

--- OOP With C++/chapter 8/Example/8.3 Multiple inheritance/main.cpp
+++ OOP With C++/chapter 8/Example/8.3 Multiple inheritance/main.cpp
@@ -1,1 +1,34 @@
+#include<iostream>
+using namespace std;
 
+class M{
+protected:
+    int m;
+public:
+    void get_m(int n){m=n;}
+};
+
+class N{
+protected:
+    int n;
+public:
+    void get_n(int m){n=m;}
+};
+
+class P : public M,public N
+{
+public :
+    void display(){
+        cout << "M : "<<m<<endl;
+        cout << "N : "<<n<<endl;
+        cout << "P : "<<m*n<<endl;
+    }
+};
+
+int main(){
+    P op;
+    op.get_m(10);
+    op.get_n(20);
+    op.display();
+
+}
