40b66bb9287703a2302d2961130ca3c2f6d8801c
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-26 19:01:26 +0530

    multilevel inheritance

--- OOP With C++/chapter 8/test/multilevel ineritance/main.cpp
+++ OOP With C++/chapter 8/test/multilevel ineritance/main.cpp
@@ -1,36 +1,13 @@
 #include<iostream>
 using namespace std;
 
-class A{
-    int a;
-public:
-    int b;
-    A(){
-        a=10;b=10;
-    }
-    int get_x(){return a;}
-    void show_x(){cout << "A : "<<a << endl;}
-};
+class student_number{
+    int roll_number;
+protected:
 
-class B : public A{
-    int c;
-public:
-    void mul(){ c = b*get_x();}
-    void show(){cout << "B : " << b << endl;
-                cout << "C : " << c << "\n\n";}
 };
 
 int main(){
-    B d;
-
-    d.show_x();
-    d.mul();
-    d.show();
-
-    d.show_x();
-    d.b=20;
-    d.mul();
-    d.show();
 
     return 0;
 }
