307964eaf2d2f188d29c105bb2e1eea984c9560f
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-27 13:49:55 +0530

    hybrid inheritance

--- OOP With C++/chapter 8/Example/8.4 Hybrid inheritance/main.cpp
+++ OOP With C++/chapter 8/Example/8.4 Hybrid inheritance/main.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 class student{
-protected:
     int roll_number;
 public:
     void get_number(int rn){roll_number=rn;}
@@ -18,13 +17,14 @@ public:
         part2=p2;
         }
     void put_marks(){
-        cout << "Marks obtain : "
+        cout << "Marks obtain : " <<endl
              << " Part1 = "<<part1<<endl
              << " Part2 = "<<part2<<endl;
         }
 };
 
 class sports{
+protected:
     float score;
 public:
     void get_score(float s){
@@ -36,10 +36,23 @@ public:
 };
 
 class result:public test,public sports{
+    float total;
+public:
+    void display(){
+        total = part1 + part2 + score;
+        put_number();
+        put_marks();
+        put_score();
 
+        cout << "Total score : "<<total <<endl;
+   }
 };
 
 int main(){
-
+    result student;
+    student.get_number(1232);
+    student.get_marks(15.20,15.20);
+    student.get_score(6.0);
+    student.display();
     return 0;
 }
