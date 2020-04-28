f0a3707afba12d5617cb435cc285017c000a92ba
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-28 15:32:34 +0530

    8.8

--- OOP With C++/chapter 8/Example/8.8 initialization list in constructors/main.cpp
+++ OOP With C++/chapter 8/Example/8.8 initialization list in constructors/main.cpp
@@ -7,7 +7,7 @@ public:
     alpha(int i):x(i){cout << "\nAlpha constructed"<<endl;}
 
     void show_alpha(){
-        cout << "X : "<< x << endl
+        cout << "X : "<< x << endl;
     }
 };
 
@@ -16,11 +16,27 @@ class beta{
 public:
     beta(float i,float j):p(i),q(j+p){cout << "\nBeta constructed"<<endl;}
 
-    void show_alpha(){
-        cout << "X : "<< x << endl
+    void show_beta(){
+        cout << "P : "<< p << endl
+             << "Q : "<< q << endl;
+    }
+};
+
+class gamma:public beta,public alpha{
+    int u,v;
+public:
+    gamma(int a,int b,float c):alpha(a*2),beta(c,c),u(a){v=b;cout << "\nBeta constructed"<<endl;}
+
+    void show_gamma(){
+        cout << "U : "<< u << endl
+             << "V : "<< v << endl;
     }
 };
 
 int main(){
+    gamma g(5,10,5.5);
+    g.show_alpha();
+    g.show_beta();
+    g.show_gamma();
     return 0;
 }
