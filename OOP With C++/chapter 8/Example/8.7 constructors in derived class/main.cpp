b2d73247438f897f58d72b82bb1737131161298e
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-28 14:37:28 +0530

    8.7

--- OOP With C++/chapter 8/Example/8.7 constructors in derived class/main.cpp
+++ OOP With C++/chapter 8/Example/8.7 constructors in derived class/main.cpp
@@ -1,6 +1,46 @@
 #include<iostream>
 using namespace std;
 
+class alpha{
+    int x;
+public:
+    alpha(int i){
+        x=i;
+        cout << "Alpha initialized \n"<<endl;
+    }
+
+    void show(){
+        cout << "X = "<<x <<endl;
+    }
+};
+
+class beta{
+    float y;
+public:
+    beta(float j){
+        y=j;
+        cout << "beta initialized \n"<<endl;
+    }
+
+    void show(){
+        cout << "Y = "<<y <<endl;
+    }
+};
+
+class gamma:public alpha,public beta{
+    int m,n;
+public:
+    alpha(int a,float b,int c,int d):alpha(a),beta(b){
+        m=c;
+        n=d;
+        cout << "Gamma initialized \n"<<endl;
+    }
+
+    void show(){
+        cout << "M = "<<m <<endl
+        cout << "N = "<<n <<endl;
+    }
+};
 
 int main(){
 
