b1bfdd1fd10b4923f32bf43e7650f1995d858499
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-28 15:20:34 +0530

    8.8

--- OOP With C++/chapter 8/Example/8.8 initialization list in constructors/main.cpp
+++ OOP With C++/chapter 8/Example/8.8 initialization list in constructors/main.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+using namespace std;
 
+class alpha{
+    int x;
+public:
+    alpha(int i):x(i){cout << "\nAlpha constructed"<<endl;}
+
+    void show_alpha(){
+        cout << ""
+    }
+};
+
+int main(){
+    return 0;
+}
