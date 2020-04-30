3d560313755ceb05c9ede4ca5a715a6e945eca9d
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-30 18:11:08 +0530

    Debugging 8.1

--- OOP With C++/chapter 8/debugging/8.1/main.cpp
+++ OOP With C++/chapter 8/debugging/8.1/main.cpp
@@ -2,30 +2,35 @@
 using namespace std;
 
 class faculty{
-private:
+protected:
     int fid;
     char name[20];
-
-    void getdata(){
+public:
+        void getdata(){
         cout <<"Enter Faculty ID : "; cin >> fid;
-        cout <<"Enter Name : "; cin >> name;
+        cout <<"Enter Name : "; cin.ignore(); cin.getline(name,20);
     }
 };
 
-class details: faculty{
-public:
+class details: private faculty{
     int age,exp;
+    public:
     void getdetails(){
         getdata();
-        cout << "Enter Age : ";cin>>age;
+        cout << "Enter Age : "; cin >> age;
         cout << "Enter Year of experience : ";cin>>exp;
     }
     void display(){
-
+        cout << "\n\nFaculty id : " << fid << endl;
+        cout << "Name of Faculty : " << name << endl;
+        cout << "Faculty Age : " << age << endl;
+        cout << "Faculty year of exp : " << exp << endl;
     }
 };
 
 int main(){
-
+    details m;
+    m.getdetails();
+    m.display();
     return 0;
 }
