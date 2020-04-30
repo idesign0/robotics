6f4e79642a75476cb3c052a50fdd9e4a8c03c927
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-30 17:51:58 +0530

    Debugging 8.1

--- OOP With C++/chapter 8/debugging/8.1/main.cpp
+++ OOP With C++/chapter 8/debugging/8.1/main.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
 
+class faculty{
+private:
+    int fid;
+    char name[20];
+
+    void getdata(){
+        cout <<"Enter Faculty ID : "; cin >> fid;
+        cout <<"Enter Name : "; cin >> name;
+    }
+};
+
 int main(){
 
     return 0;
