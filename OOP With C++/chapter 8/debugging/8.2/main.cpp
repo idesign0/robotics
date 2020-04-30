4d078343bc1741d056eaca586f1155aeaabba044
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-04-30 20:32:29 +0530

    Debugging 8.2

--- OOP With C++/chapter 8/debugging/8.2/main.cpp
+++ OOP With C++/chapter 8/debugging/8.2/main.cpp
@@ -1,7 +1,31 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+class four_seater{
+public:
+    void property(){
+        cout << "It has space for four persons" << endl;
+    }
+};
 
+class four_wheeler{
+public:
+    void property(){
+        cout << "It runs on four types" << endl;
+    }
+};
+
+class car:public four_seater,public four_wheeler{
+public:
+    void display(){
+        four_seater::property();
+        four_wheeler::property();
+    }
+
+};
+
+int main(){
+    car c1;
+    c1.display();
     return 0;
 }
