a3610f71e753a93ddfd08ec94a44e5ab4fef5efd
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-05-01 20:12:13 +0530

    programming exercises 8.1

--- OOP With C++/chapter 8/Programming Exercises/8.1 current and saving account class/main.cpp
+++ OOP With C++/chapter 8/Programming Exercises/8.1 current and saving account class/main.cpp
@@ -3,9 +3,20 @@ using namespace std;
 
 class account{
 protected:
-    char * customer_name = new char[20];
-    char * type_of_account =  new char[10];
+    string customer_name ;
+    string type_of_account ;
     int account;
+public:
+    void add_details(){
+        int choice;
+        cin.ignore();
+        cout << "Enter customer name : " ; getline(cin,customer_name);
+        cout << "Enter type of account : " << endl
+             << "Press 1 for Current account" << endl
+             << "Press 2 for Savings account" << endl;
+        cin >> choice ;
+        if(choice==1) ? type_of_account="current" : type_of_account="savings";
+    }
 };
 
 int main(){
