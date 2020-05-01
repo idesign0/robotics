19ae799530042766ed55620a8159af1d35a71af9
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-05-01 19:26:06 +0530

    programming exercises 8.1

--- OOP With C++/chapter 8/Programming Exercises/8.1 current and saving account class/main.cpp
+++ OOP With C++/chapter 8/Programming Exercises/8.1 current and saving account class/main.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 using namespace std;
 
+class account{
+protected:
+    char * customer_name;
+    char * type_of_account
+    int account;
+public:
+
+};
+
 int main(){
 
     return 0;
