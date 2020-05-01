6792cbcbac69fe589ce7c4410c2fe77b29e6d270
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-05-01 14:03:10 +0530

    Debugging 8.4

--- OOP With C++/chapter 8/debugging/8.5/main.cpp
+++ OOP With C++/chapter 8/debugging/8.5/main.cpp
@@ -1,7 +1,34 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+class p{
+protected:
+    int i;
+};
 
+class q:virtual public p{
+protected:
+    int j;
+};
+
+class r:virtual public p{
+protected:
+    int k;
+};
+
+class s:public q,public r{
+private:
+    int mul;
+public:
+    int multi_fun(){
+        i=10;j=20;k=30;
+        mul = i*j*k;
+        return mul;
+    }
+};
+
+int main(){
+    s ob1;
+    cout << "Multiplication is : " << ob1.multi_fun() << endl;
     return 0;
 }
