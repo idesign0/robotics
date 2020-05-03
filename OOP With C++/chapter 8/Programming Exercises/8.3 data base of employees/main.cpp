f3ff724b5f91b953688958a3ac2a98c811fe38b6
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-05-03 16:30:21 +0530

    programming exercises 8.3

--- OOP With C++/chapter 8/Programming Exercises/8.3 data base of employees/main.cpp
+++ OOP With C++/chapter 8/Programming Exercises/8.3 data base of employees/main.cpp
@@ -7,24 +7,29 @@ class staff{
 protected:
     int code;
     string name;
-    char* position[10];
+    enum position{officer=1,teacher,regular,casual};
+    int choice;
 public:
     void get_details(){
         cout << "Enter code of Staff member : "; cin >> code;
         cin.ignore();
         cout << "Enter name of Staff member : "; getline(cin,name);
-        cout << "Enter position of Staff member : "; cin>>position;
+        cout << "Enter position of Staff member Enter option between 1 to 4\n(1)officer (2)Teacher (3)Regular (4)Casual) : "; cin>>choice;
     }
     void show_details(){
-        cout << "Code of Staff member : "<<code;
-        cout << "Name of Staff member : "<<name;
-        cout << "Position of Staff member : "<<position;
+        cout << "Code of Staff member : "<<code<<endl;
+        cout << "Name of Staff member : "<<name<<endl;
     }
+    friend int searchh(string &,int);
 };
+
+int searchh(string &a,int b){
+
+}
 
 // officer class
 
-class officer:public staff{
+class officer:virtual public staff{
 protected:
     char grade;
 public:
@@ -32,71 +37,126 @@ public:
         cout << "Enter Grade of Officer : "; cin >> grade;
     }
     void show_details(){
-        cout << "Grade of Officer : " << grade;
+        cout << "Grade of Officer : " << grade<<endl;
         }
 };
 
 // teacher class
 
-class teacher:public staff{
+class teacher:virtual public staff{
 protected:
     string subject;
     string publication;
 public:
     void get_details(){
+        cin.ignore();
         cout << "Enter subject of teacher : "; getline(cin,subject);
         cout << "Enter name of publications : "; getline(cin,publication);
     }
     void show_details(){
-        cout << "Enter subject of teacher : "<<subject;
-        cout << "Enter name of publications : "<<publication;
+        cout << "Subject of teacher : "<<subject<<endl;
+        cout << "Name of publications : "<<publication<<endl;
     }
 };
 
 // typist class
 
-class typist:public staff{
+class typist:virtual public staff{
 protected:
     int speed;
 public:
     void get_details(){
-        cout << "Enter the speed of typist : "; cin>>speed;
+        cout << "Enter the speed of typist (words per sec): "; cin>>speed;
         }
     void show_details(){
-        cout << "Speed of typist : "<<speed;
+        cout << "Speed of typist : "<<speed<< " words per sec"<<endl;
     }
 };
 
 // regular class
 
-class regular:public typist{
-
+class regular:virtual public typist{
+    public:
+    void get_details(){
+        typist::get_details();
+    }
+    void show_details(){
+      typist::show_details();
+    }
 };
 
 // casual class
 
-class casual:public typist{
+class casual:virtual public typist{
 protected:
     float daily_wages;
 public:
     void get_details(){
-        cout << "Enter daily wages of casual typist: "; cin>>daily_wages;
+        typist::get_details();
+        cout << "Enter daily wages of casual typist (dollars per day): "; cin>>daily_wages;
+
         }
     void show_details(){
-        cout << "Daily wages of typist : "<<daily_wages;
+        typist::show_details();
+        cout << "Daily wages of typist : "<<daily_wages<<" dollars per day"<<endl;
     }
 };
 
-class employees{
+class employees:public teacher,public regular,public casual,public officer{
 public:
-     void add_details(){
+     void get_details(){
+        cout << "\nAdd details of Employee : \n\n";
+        staff::get_details();
+        switch(choice){
+            case 1 : officer::get_details();break;
 
+            case 2 : teacher::get_details();break;
+
+            case 3 : regular::get_details();break;
+
+            case 4 : casual::get_details();break;
+
+        }
+     }
+
+    void show_details(){
+        cout << "\nshow details of Employee : \n\n";
+        staff::show_details();
+        switch(choice){
+            case 1 : officer::show_details();break;
+
+            case 2 : teacher::show_details();break;
+
+            case 3 : regular::show_details();break;
+
+            case 4 : casual::show_details();break;
+
+        }
      }
 
 };
 
 int main(){
-    casual c;
-    c.get_details();
+    employees *emp1;
+    bool value = true;
+    while(value){
+        emp1 = new employees[members];
+        int option,members=0;
+        string name;int account_number;
+        cout << "Menu : \n\n"
+             << "\n1.Add details of employees"
+             << "\n2.Show details of employees"
+             << "\n3.Quite"
+             << "\nYour option : ";cin>>option;
+
+         switch(option){
+            case 1 : emp1[members].get_details();
+                     members++;break;
+            case 2 : cin.ignore();
+                     cout << "Enter name of the Employee : "; getline(cin,name);
+                     cout << "Enter a/c number of the Employee : "; cin>>account_number;
+         }
+
+    }
     return 0;
 }
