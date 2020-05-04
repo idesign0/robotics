f29815d2323fca457c6eabba1739f4b5c2fe2979
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-05-04 18:18:23 +0530

    8.5

--- OOP With C++/chapter 8/Programming Exercises/8.5 hybrid inheritance system/main.cpp
+++ OOP With C++/chapter 8/Programming Exercises/8.5 hybrid inheritance system/main.cpp
@@ -7,16 +7,18 @@ protected:
     int code;
 public:
     void get_details(){
+    cin.ignore();
     cout << "Name of Person : ";getline(cin,name);
     cout << "Enter code : "; cin >>code;
     }
     void update_details(){
+    cin.ignore();
     cout << "Name of Person : ";getline(cin,name);
     cout << "Enter code : "; cin >>code;
     }
     void show_details(){
-    cout << "Name of Person : "<<name;
-    cout << "Enter code : "<< code;
+    cout << "Name of Person : "<<name<<endl;
+    cout << "Enter code : "<< code<<endl;
     }
 };
 
@@ -31,7 +33,7 @@ public:
     cout << "pay of the person : ";cin>> pay;
     }
     void show_details(){
-    cout << "pay of the person : "<<pay;
+    cout << "pay of the person : "<<pay<<endl;
     }
 };
 
@@ -41,15 +43,17 @@ protected:
 public:
     void get_details(){
     person::get_details();
+    cin.ignore();
     cout << "Experience : ";getline(cin,experience);
     }
     void update_details(){
     person::update_details();
+    cin.ignore();
     cout << "Experience : ";getline(cin,experience);
     }
     void show_details(){
     person::show_details();
-    cout << "Experience : "<<experience;
+    cout << "Experience : "<<experience<<endl;
     }
 };
 
@@ -67,10 +71,70 @@ public:
         admin::show_details();
         account::show_details();
     }
+    int searchh(string a,int b){
+        if(a.compare(name)==0&&b==code){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
 
 };
 
 int main(){
+    master m1[20];
+    bool value = true;
+    int member=0;
+    while(value){
 
+        int option,i=0;
+        string name;int code_number;
+        cout << "Menu : \n"
+             << "\n1.Add details of employees"
+             << "\n2.Update details of employees"
+             << "\n3.Show details of employees"
+             << "\n4.Quite"
+             << "\nYour option : ";cin>>option;
+
+         switch(option){
+            case 1 : cout << "\n*****************************************************************\n";
+                        m1[member].get_details();
+                    cout << "\n*****************************************************************\n";
+                     member++;break;
+            case 2 :
+                     cin.ignore();
+                     cout << "Enter name of the Employee : "; getline(cin,name);
+                     cout << "Enter code number of the Employee : "; cin>>code_number;
+
+                     for(i=0;i<member;i++){
+                        if(m1[i].searchh(name,code_number)==1){
+                            cout << "\n*****************************************************************\n";
+                            m1[i].update_details();
+                            cout << "\n*****************************************************************\n";
+                            break;
+                        }
+                     }
+                     if(i==member){"employees are not listed\n";}break;
+            case 3 :
+                     cin.ignore();
+                     cout << "Enter name of the Employee : "; getline(cin,name);
+                     cout << "Enter code number of the Employee : "; cin>>code_number;
+
+                     for(i=0;i<member;i++){
+                        if(m1[i].searchh(name,code_number)==1){
+                            cout << "\n*****************************************************************\n";
+                            m1[i].show_details();
+                            cout << "\n*****************************************************************\n";
+                            break;
+                        }
+                     }
+                     if(i==member){"employees are not listed\n";}break;
+            case 4 : value = false;
+
+            default : cout << "Invalid Choice"<< endl;
+         }
+
+    }
     return 0;
 }
+
