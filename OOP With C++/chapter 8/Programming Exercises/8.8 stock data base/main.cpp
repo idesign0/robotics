4ebe39c7ef50361a20db69cee2164b301487bb89
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-05-05 14:05:36 +0530

    8.8

---
+++ OOP With C++/chapter 8/Programming Exercises/8.8 stock data base/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class kitchen_product{
+    int code;
+    string name;
+    enum type{electronics_appliances,food_items,milk_product};
+    int choice;
+};
+
+class electronic{
+
+};
+
+int main(){
+
+    return 0;
+}
