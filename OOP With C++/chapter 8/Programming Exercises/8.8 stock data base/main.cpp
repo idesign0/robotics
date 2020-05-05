efbcbc9e8f70cb534250d6b1207ecc8ead37df95
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-05-05 14:48:05 +0530

    8.8

--- OOP With C++/chapter 8/Programming Exercises/8.8 stock data base/main.cpp
+++ OOP With C++/chapter 8/Programming Exercises/8.8 stock data base/main.cpp
@@ -1,15 +1,56 @@
 #include<iostream>
 using namespace std;
 
-class kitchen_product{
+class menu{
     int code;
-    string name;
-    enum type{electronics_appliances,food_items,milk_product};
-    int choice;
+    enum product_type{electronic,food,milk,cloths};
+public:
+    void get_code(){
+    cout << "Press appropriate Number associated with the product : "
+         << "\n1. Electronics appliances "
+         << "\n2. Food "
+         << "\n3. Milk products "
+         << "\n4. Clothes "
+         << "\nEnter your choice : ";cin>>code;
+    }
 };
 
-class electronic{
+class electronic_product:public menu{
+    string name;
+    float price;
+    int stock;
+public:
+    void get_details(){
+    cin.ignore();
+    cout << " Name of electronic Product : ";getline(cin,name);
+    cout << " Price : ";cin>>price;
+    cout << " Stock : ";cin>>stock;
+    }
+    void show_details(){
+    cin.ignore();
+    cout << " Name of electronic Product : "<< name << endl;
+    cout << " Price : "<< price << endl;
+    cout << " Stock : "<< stock;
+    }
+};
 
+class kitchen_product:public menu{
+    string name;
+    float price;
+    int stock;
+public:
+    void get_details(){
+    cin.ignore();
+    cout << " Name of electronic Product : ";getline(cin,name);
+    cout << " Price : ";cin>>price;
+    cout << " Stock : ";cin>>stock;
+    }
+    void show_details(){
+    cin.ignore();
+    cout << " Name of electronic Product : "<< name << endl;
+    cout << " Price : "<< price << endl;
+    cout << " Stock : "<< stock;
+    }
 };
 
 int main(){
