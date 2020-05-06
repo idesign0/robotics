efed22f3421f3b507bb5f3a85445f0b07dafaf22
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-05-06 15:30:40 +0530

    8.9

--- OOP With C++/chapter 8/Programming Exercises/8.9 function and classes as relation with shopkeeper/main.cpp
+++ OOP With C++/chapter 8/Programming Exercises/8.9 function and classes as relation with shopkeeper/main.cpp
@@ -7,7 +7,7 @@ class cars{
     int choices;
     enum car{two=1,three,four};
 public:
-    cars(){
+    void get_details(){
         cin.ignore();
         cout << "Enter name of the Model : ";getline(cin,name);
         cout << "Enter code of the Vehicle model : ";cin>>code;
@@ -16,6 +16,31 @@ public:
              << "2.Three wheeler\n"
              << "3.Four wheeler\n"
              << "\nChoose Appropriate option : ";cin>>choices;
+    }
+
+    void show_details(){
+        cout << "Name of the Model : " << name << endl;
+        cout << "Code of the Model : " << code << endl;
+    }
+};
+
+class two::public virtual cars{
+    float power;
+    string tyre;
+    string gear;
+    float weight;
+    void get_details(){
+        cin.ignore();
+        cout << "Power developed by two wheeler engine : ";cin>>power;
+        cout << "Tyre type of two wheeler engine : ";getline(cin,tyre);
+        cout << "Gear : ";getline(cin,gear);
+        cout << "Weight : ";cin>>weight;
+    }
+    void show_details(){
+        cout << "Power developed by two wheeler engine : "<<power<<endl;
+        cout << "Tyre type of two wheeler engine : ";<<tyre<<endl;
+        cout << "Gear : "<<gear<<endl;
+        cout << "Weight : "<<weight<<endl;
     }
 };
 
