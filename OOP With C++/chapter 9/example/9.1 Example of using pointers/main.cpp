699e5afe54ae67170374546c02bdee9279931200
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-05-07 19:26:03 +0530

    9.1

--- OOP With C++/chapter 9/example/9.1 Example of using pointers/main.cpp
+++ OOP With C++/chapter 9/example/9.1 Example of using pointers/main.cpp
@@ -1,11 +1,20 @@
-#include <iostream>
-#include <ctime>
-#include <cstdlib>
+#include <iostream>>
  using namespace std;
 
  int main(){
+    int b,*ptr1,**ptr2;
+    ptr1=&b;
+    ptr2=&ptr1;
 
+    cout << "The address of a : "<<ptr1<<"\n";
+    cout << "The address of ptr1 : "<<ptr2<<"\n";
+
+    cout << "\n\n";
+    cout << "After incrementing the address value : \n\n";
+    ptr1+=2;
+    cout << "The address of a : "<<ptr1<<"\n";
+    ptr2+=2;
+    cout << "The address of ptr1 : "<<ptr2<<"\n";
 
     return 0;
-
  }
