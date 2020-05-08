e88b72bf73299fbc00c2a3a877200b6129c30754
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-05-08 13:48:58 +0530

    9.3

---
+++ OOP With C++/chapter 9/example/9.3 Arithmetic operators in pointers/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num[]={58,15,61,58,45,20};p
+    int *ptr;
+    int i;
+
+    cout << "The array values are : \n";
+    for(i=0,i<=5,i++){
+        cout << num[i]<<endl;
+    }
+    return 0;
+}
