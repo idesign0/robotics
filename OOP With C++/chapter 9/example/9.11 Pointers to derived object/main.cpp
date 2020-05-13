657b043e72b6265776adf399080c3624a3881706
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-05-13 18:49:07 +0530

    9.10

---
+++ OOP With C++/chapter 9/example/9.11 Pointers to derived object/main.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+class bc{
+public:
+    int b;
+    void show(){
+    cout << "b = "<<b<<endl;
+    }
+};
+class dc:public bc{
+public:
+    int d;
+    void show(){
+    cout << "b = "<<b<<endl;
+    cout << "d = "<<d<<endl;
+    }
+};
+
+int main(){
+    bc *bptr;
+    bc base;
+    bptr=&base;
+
+    bptr->b=100;
+    cout << "bptr points to base objects \n";
+    bptr->show();
+
+    //derived class
+
+    dc derived;
+    bptr=&derived;
+    bptr->b=200;
+
+    cout << "bptr now points to derived objects \n";
+    bptr->show();
+
+    dc *dptr;
+    dptr=&derived;
+
+    dptr->d=300;
+    cout << "dptr is derived type pointer \n";
+    dptr->show();
+
+    cout << "Using (dc*)bptr : \n";
+    ((dc*)bptr) -> d = 400;
+    ((dc*)bptr) ->show() ;
+    return 0;
+
+}
