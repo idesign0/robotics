c0940abf92ed56da4af4d88b5137fcd15e758343
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-05-13 09:49:54 +0530

    9.8

---
+++ OOP With C++/chapter 9/example/9.7 Pointers to objects/main.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+class item{
+    int code;
+    float price;
+public:
+    void getdata(int a,float b){
+        code =a;
+        price=b;
+    }
+    void show(void){
+        cout << "Code : "<< code << endl
+             << "Price : "<< price << endl;
+    }
+};
+
+const int sizee =2;
+int main(){
+    item *p = new item[sizee];
+    item *d = p;
+    int x,i;
+    float y;
+
+    for(i=0;i<sizee;i++){
+        cout <<"Input code and price for item"<<i+1<<" ";
+        cin>>x>>y;
+        p->getdata(x,y);
+        p++;
+    }
+
+    for(i=0;i<sizee;i++){
+        cout <<"Item"<<i+1<<" ";
+        d->show();
+        d++;
+    }
+    return 0;
+}
