86f2169e7fa06306fbce36ac2b9e2d46b6f6e483
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-05-17 09:13:14 +0530

    9.

---
+++ OOP With C++/chapter 9/debugging/9.6/main.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+class original_base{
+public:
+    virtual void print(){
+        cout << "Print version of base class\n";
+    }
+    void show(){
+        cout << "Show version of base class\n";
+    }
+};
+
+class derived_class:public original_base{
+public:
+    void print(){
+        cout << "Print version of derived class\n";
+    }
+    void show(){
+        cout << "Show version of derived class\n";
+    }
+};
+
+int main(){
+    original_base *ptr;
+    derived_class x;
+    ptr=&x;
+    ptr->print();
+    ptr->show();
+    return 0;
+}
