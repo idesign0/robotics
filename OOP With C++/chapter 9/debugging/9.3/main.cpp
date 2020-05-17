0f713dcb582ce87770201f1a72603653614c0c31
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-05-17 08:49:10 +0530

    9.1

---
+++ OOP With C++/chapter 9/debugging/9.3/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int p=10;
+    double q=20.33;
+    int *x = &p;
+    double *y=&q;
+
+    cout << "Value 1 : " << *x << endl;
+    cout << "Value 2 : " << *y << endl;
+    return 0;
+}
