bc06b4a79bec619d0594482ede9907f7b6788441
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-05-17 10:16:22 +0530

    9.2

--- OOP With C++/chapter 9/Programming exercises/9.2 Extending area class with circles/main.cpp
+++ OOP With C++/chapter 9/Programming exercises/9.2 Extending area class with circles/main.cpp
@@ -8,7 +8,7 @@ protected:
     double height;
     double area;
 public:
-    void get_data(double w,double h){
+    void get_data(double w,double h=0){
         width =w;
         height = h;
     }
@@ -31,18 +31,29 @@ public:
     }
 };
 
+class circle:public shape{
+public:
+    void display_area(){
+        area = 3.14159*pow(width,2);
+                cout << "Area of Rectangle : " << area << endl;
+    }
+};
+
 
 int main(){
     triangle o_triangle;
     rectangle o_rectangle;
+    circle o_circle;
 
     o_triangle.get_data(5,5);
     o_rectangle.get_data(10,10);
+    o_circle.get_data(10);
 
-    shape *sptr[2]={&o_rectangle,&o_triangle};
+    shape *sptr[3]={&o_rectangle,&o_triangle,&o_circle};
 
     sptr[0]->display_area();
     sptr[1]->display_area();
+    sptr[2]->display_area();
 
     return 0;
 }
