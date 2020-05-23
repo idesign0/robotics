aba4b31f995dc4e58268941c05ee1b5b4ccef772
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-05-23 17:32:00 +0530

    10.1 programming exercise

--- OOP With C++/chapter 10/Programming Exercises/10.1 Read and diplay in colums/main.cpp
+++ OOP With C++/chapter 10/Programming Exercises/10.1 Read and diplay in colums/main.cpp
@@ -1,11 +1,65 @@
 #include<iostream>
+#include<iomanip>
+#include<stdlib.h>
 using namespace std;
 
+class softwares{
+    string name;
+    int code;
+    float cost;
+public:
+    softwares();
+    void display();
+};
+
+ostream & form(ostream & output){
+    output.precision(2);
+    output.setf(ios::showpoint);
+    output<<setiosflags(ios::fixed)<<setw(10);
+    return output;
+}
+
+ostream & space(ostream & output){
+    output<<setw(10);
+    return output;
+}
+
+softwares::softwares(){
+    cin.ignore();
+    cout << "Enter Software name : ";getline(cin,name);
+    cout << "Enter Software code : ";cin>>code;
+    cout << "Enter Software cost : ";cin>>cost;
+}
+
+void softwares::display(){
+    cout <<form << name << space << code << space << cost <<endl;
+}
 int main(){
     const int sizee=10;
-    string name[sizee];
-    int code[sizee];
-    int cost[sizee];
+    softwares *o_softwares[sizee];
 
+    int countt=0, i;
+    int option;
+    cout << "Display Information of software :\n";
+    while(1){
+        cout << "\nMenu";
+        cout << "\n1.Add Details";
+        cout << "\n2.Show Details";
+        cout << "\n3.Quite";
+        cout << "\nOption : ";
+        cin>>option;
+
+        switch(option){
+            case 1 : o_softwares[countt]=new softwares;
+                     o_softwares[countt];
+                     countt++;break;
+            case 2 :
+                    cout<<endl<<space<<"NAME"<<space<<"CODE"<<space<<"COST"<<"\n";
+                    for(i=0;i<countt;i++){
+                        o_softwares[i]->display();
+                    }
+            case 3 : exit(0);
+        }
+    }
     return 0;
 }
