555ee58ece8d92fc668d4d2480458276192f2a6e
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-05-24 10:26:40 +0530

    10.3 programming exercise

--- OOP With C++/chapter 10/Programming Exercises/10.3 Read text and give details/main.cpp
+++ OOP With C++/chapter 10/Programming Exercises/10.3 Read text and give details/main.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
 #include<cstring>
+#include<iomanip>
 using namespace std;
+
+ostream & margin(ostream & output){
+    output.width(20);
+    output.setf(ios::right,ios::adjustfield);
+    return output;
+}
+
+ostream & right_padding(ostream & output){
+    output.width(20);
+    output.setf(ios::left,ios::adjustfield);
+    return output;
+}
 
 int main(){
     char sentence[20];
@@ -31,10 +44,8 @@ int main(){
 
     character =character-space;
 
-    cout << "Words : " << words <<" ";
-    cout << "New line : " << new_line<<" ";
-    cout << "Space : "<<space<< " ";
-    cout << "Character : " << character<<" ";
+    cout <<right_padding<<"Name of String"<<margin<< "Number of Lines"<<margin<<"Number of Words" <<margin<<"Number of character" << endl;
+    cout <<right_padding<<sentence<<margin<< new_line<<margin<<words <<margin<<character<< endl;
 
     return 0;
 }
