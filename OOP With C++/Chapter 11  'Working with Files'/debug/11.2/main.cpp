17d102f79cfc40d8d14938db9461302d83cbac7f
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-05-31 14:33:43 +0530

    new beginning

--- OOP With C++/Chapter 11  'Working with Files'/debug/11.2/main.cpp
+++ OOP With C++/Chapter 11  'Working with Files'/debug/11.2/main.cpp
@@ -5,31 +5,21 @@
 using namespace std;
 
 int main(){
-    const int sizee=100;
-    char buf[sizee];
-    int i=0;
+    char buf[80];
+    ifstream fin;
+    ofstream fout("chll_2.cpp");
 
-    ifstream in("chll_1.text");
-    ofstream out("chll_1temp.text");
+    fout << "aasdasdasdas";;
+    fout.close();
 
-    if(in.fail()){
-            cout << "failed _ in";
-            exit(1);
+    fin.open("chll_2.cpp");
+
+    if(fin.fail()){
+        cout << "....";
+        exit(1);
     }
 
-    if(out.fail()){
-            cout << "failed _ out";
-            exit(1);
-    }
-
-    in.getline(buf,50);
-
-    in.clear();
-    out.clear();
-
-    for(int i=0;i<strlen(buf);i++){
-        out.put(buf[i]);
-     }
-
+    fin.getline(buf,80);
+    cout<<buf;
     return 0;
 }
