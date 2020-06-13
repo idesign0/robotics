0a36dd16d9a5731d2a1874dc8998bb6f98a20833
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-06-13 13:39:15 +0530

    Programming exercise 13.5

--- OOP With C++/Chapter 13 'Exception Handling'/Programming Exercises/13.5 Retrowing an Exception/main.cpp
+++ OOP With C++/Chapter 13 'Exception Handling'/Programming Exercises/13.5 Retrowing an Exception/main.cpp
@@ -1,9 +1,29 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    int a,b;
-    float r;
+int minuus(int a,int b){
+    try{
+        if(!(a-b)){
+            throw 0;
+        }else{
+            return a-b;
+        }
+        }catch(int a){
+            cout << "Dividend is Zero ! \n";
+            throw;
+        }
 
+}
+
+int main(){
+    int a=5,b=3;
+    float r;
+    try{
+        int division = minuus(a,b);
+        r = (float)a/division;
+        cout << "Ratio : " << r;
+    }catch(int a){
+        cout << "Control goes to main !";
+    }
     return 0;
 }
