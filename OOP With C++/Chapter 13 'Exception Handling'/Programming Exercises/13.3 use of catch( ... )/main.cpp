0899fe915aa208055a88266562c700f7d9320d2f
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-06-13 13:16:38 +0530

    Programming exercise 13.3

---
+++ OOP With C++/Chapter 13 'Exception Handling'/Programming Exercises/13.3 use of catch( ... )/main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+const int sizee=5;
+int main(){
+    int a,b;
+    float r;
+
+    cout << "Enter values of a and b : " ; cin >>a>>b;
+    try{
+        if(!(a-b)){
+            throw 0;
+        }else if((a-b)==1)
+            {
+                throw 'c';
+            }
+        else if((a-b)<0)
+            {
+                throw 1.0;
+            }
+        else{
+            r = (float)a/(a-b);
+            cout << "Ratio : " << r << endl;
+        }
+    }
+    catch(...){
+        cout << "Exception is caught !";
+    }
+    return 0;
+}
