21c97ce429a00658d0dcdfb2f9e3c4564d59b996
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-06-13 13:23:05 +0530

    Programming exercise 13.4

---
+++ OOP With C++/Chapter 13 'Exception Handling'/Programming Exercises/13.4 Demonstrates how certain exception types are not allowed to be thrown/main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+const int sizee=5;
+int main() throw(int,double){
+    int a,b;
+    float r;
+
+    cout << "Enter values of a and b : " ; cin >>a>>b;
+    try{
+        if(!(a-b)){
+            throw 0;
+        }else if((a-b)==1)
+            {
+                throw 'c';
+            }
+        else if((a-b)<0)
+            {
+                throw 1.0;
+            }
+        else{
+            r = (float)a/(a-b);
+            cout << "Ratio : " << r << endl;
+        }
+    }
+    catch(int a){
+        cout << "Exception is caught : division is 0 !";
+    }
+    catch(double a){
+        cout << "Exception is caught : division is minus  !";
+    }
+    catch(...){
+        cout << "Exception is caught !";
+    }
+    return 0;
+}
