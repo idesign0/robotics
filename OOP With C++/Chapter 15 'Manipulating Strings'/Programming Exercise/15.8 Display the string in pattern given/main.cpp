785ffe5768de380260f3c6a4e2e9fba3f2fb7e5b
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-06-24 17:02:41 +0530

    programming exercise 15.8

--- OOP With C++/Chapter 15 'Manipulating Strings'/Programming Exercise/15.8 Display the string in pattern given/main.cpp
+++ OOP With C++/Chapter 15 'Manipulating Strings'/Programming Exercise/15.8 Display the string in pattern given/main.cpp
@@ -5,35 +5,36 @@
 using namespace std;
 
 int main(){
-    string a("123456789");
-    string pattern[5];
-    string final_pattern[5];
+    string a("1234567891011121314");
+    const int s = (a.size())/2+1;
+    string pattern[s];
+    string final_pattern[s];
 
-    for(int i=0;i<5;i++){
+    for(int i=0;i<s;i++){
         for(int j=i;j<=(2*i);j++){
             pattern[i].push_back(a.at(j));
             pattern[i].push_back(' ');
         }
     }
 
-    for(int i=0;i<5;i++){
-        for(int j=0;j<2*(5-i);j++){
+    for(int i=0;i<s;i++){
+        for(int j=0;j<=2*((s-1)-i);j++){
             pattern[i].insert(pattern[i].begin(),' ');
             final_pattern[i]=pattern[i];
     }
     }
 
-    for(int i=0;i<5;i++){
+    for(int i=0;i<s;i++){
             reverse(pattern[i].begin(),pattern[i].end());
             pattern[i].erase(pattern[i].begin(),pattern[i].begin()+3);
         }
 
 
-    for(int i=0;i<5;i++){
+    for(int i=0;i<s;i++){
             final_pattern[i].append(pattern[i]);
         }
 
-    for(int i=0;i<5;i++){
+    for(int i=0;i<s;i++){
         cout << final_pattern[i] << endl;
     }
 
