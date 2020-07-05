55d6d8a52c09ccbf745d4b0fe78f5f6b9bd5bd3f
Dhruv Patel <dhruvpatel2991998@gmail.com>
2020-07-05 15:51:33 +0530

    Projects - minor project - CMS

--- OOP With C++/Projects/A.1 Minor Project - College Management System (CMS)/main.cpp
+++ OOP With C++/Projects/A.1 Minor Project - College Management System (CMS)/main.cpp
@@ -1,1 +1,12 @@
+// Learning objectives
+// Develop menu-driven applications in c++
+// Apply authentication in menu-driven projects
+// Apply file handling concepts
 
+/* CMS application code3 makes use of a wide array of C++ programming features : */
+
+// concept of inheritance for code re-usability
+// concept of function overloading that enhances the code readability
+// handling simple exception
+// applications for file handling concepts for performing operations, such as read, write, append, check for various i/o errors, etc
+// use of various i/o manipulators to display output in formatted manner
