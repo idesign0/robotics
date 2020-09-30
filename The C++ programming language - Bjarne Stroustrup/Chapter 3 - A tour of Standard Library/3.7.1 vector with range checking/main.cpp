2940042d1434f09c3f78f508c2efa2a60a7e062e
idesign0 <dhruvpatel2991998@gmail.com>
2020-09-30 15:03:50 +0530

    3.7.2

---
+++ The C++ programming language - Bjarne Stroustrup/Chapter 3 - A tour of Standard Library/3.7.1 vector with range checking/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template<class T>
+class Vec : public vector<T>{ // Vec class for handling exception
+public :
+    Vec():vector<T>(){}
+    Vec(int s):vector<T>(s){}
+
+    T & operator[](int i){
+        return this->at(i);
+    }
+
+    const T & operator[](int i) const {
+        return this->at(i);
+    }
+
+};
+
+struct Entry{
+    string name;
+    int numbers;
+
+    void fill_up(string name,int num){
+        this->name=name;
+        numbers=num;
+    }
+
+    void print_name(){
+        cout << this->name << " " <<this->numbers << "\n";
+    }
+};
+
+
+
+int main()
+{
+    try{
+        Vec<Entry> phone_book(1000);
+        phone_book[0].fill_up("xyz",100);
+        phone_book[0].print_name();
+
+        }catch(out_of_range){
+        cout << "Range error\n";
+
+        }catch(...){
+        cout << "unknown Exception Thrown\n";
+        }
+
+        return 0;
+}
