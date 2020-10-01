c72df1c9426053f9df9edfae3342f42ac7d407fa
idesign0 <dhruvpatel2991998@gmail.com>
2020-10-01 17:17:12 +0530

    3.8 count

---
+++ The C++ programming language - Bjarne Stroustrup/Chapter 3 - A tour of Standard Library/3.8 iterators/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+template<class C,class t>
+int countt(const C &v, t val){
+    typename C::const_iterator i=find(v.begin(),v.end(),val);
+    int n=0;
+    while(i !=v.end()){
+        ++n;
+        ++i;
+        i=find(i,v.end(),val);
+    }
+
+    return n;
+}
+
+int main()
+{
+    string s = "Mary had a little lamp!";
+    int i = countt(s,'a');
+
+    cout << i << endl;
+
+    vector<string> vc(5);
+    vc.push_back("hii");
+    vc.push_back("dhruv");
+
+    int j = count(vc.begin(),vc.end(),"dhruv");
+    cout << j << endl;
+    return 0;
+}
