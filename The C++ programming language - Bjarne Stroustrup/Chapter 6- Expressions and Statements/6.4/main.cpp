87249f9ba322f4a8e5680dbdb73ea75e59cf5517
idesign0 <dhruvpatel2991998@gmail.com>
2020-10-13 16:00:02 +0530

    6.5

---
+++ The C++ programming language - Bjarne Stroustrup/Chapter 6- Expressions and Statements/6.4/main.cpp
@@ -0,0 +1,37 @@
+// bitwise logical operators combination
+
+//  bitwise-and &
+// if two values are true then and then result value will be true
+/*
+
+value1 & value2 result
+1        1      1
+1        0      0
+0        1      0
+0        0      0
+
+*/
+
+//  bitwise-or |
+// if atleast one value is true then result value will be true.
+/*
+
+value1 & value2 result
+1        1      1
+1        0      1
+0        1      1
+0        0      0
+
+*/
+
+//  bitwise-exclusive-or ^
+// if two values are same then result value will be false.
+/*
+
+value1 & value2 result
+1        1      0
+1        0      1
+0        1      1
+0        0      0
+
+*/
---
+++ The C++ programming language - Bjarne Stroustrup/Chapter 6- Expressions and Statements/6.5/main.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<limits>
+using namespace std;
+
+int main(){
+
+/////////////////////// implementation defined behavior
+    // differ from system to system
+    cout << (sizeof(long long) > sizeof(long)) << endl;
+    cout << sizeof(wchar_t)<<endl;
+    int s$;
+    cout << s$ << endl;
+    cout << numeric_limits<int>::max()+1 << endl;
+
+    int arrr[10];
+    for(int i=0;i<10;i++){
+        cout << arrr[i] << endl; // random values
+    }
+
+    // bool uninitialized
+    bool d;
+    (d==true) ? cout << "True" << endl : cout << "False" << endl; // random value
+
+    // undefined typecasting
+    long x = -1;
+    char y = char(x);
+    cout << y << endl; //undefined
+
+///////////////////////undefined behavior
+
+    // dereferencing null pointer
+    char *p = 0;
+    cout << *p << endl; //runtime error
+
+    // dividend 0
+    int a = 1;int b=0;
+    int c = a/b;
+    cout << c << endl; //  runtime error
+
+    // 7.string literal
+    char *str = "hello world";
+    str[0]='b';
+    str[4]='p';
+    cout << str << endl; // runtime error
+
+}
