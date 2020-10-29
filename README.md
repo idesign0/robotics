69a7961a563276fbf021814c0b4577d4c45f3eb8
idesign0 <dhruvpatel2991998@gmail.com>
2020-10-29 18:29:52 +0530

    read me update

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Started with Programming basic in C/C++. Now concentrating more in OOP with C++.
 
 Refrence 
 - OOP with C++ by E Balagurusamy
-- The c++ programming language - Book by Bjarne Stroustrup
+- The c++ programming language - Book by Bjarne Stroustrup.
 
 
--- The C++ programming language - Bjarne Stroustrup/Chapter 3 - A tour of Standard Library/3.8.4 traversals and predicates/main.cpp
+++ The C++ programming language - Bjarne Stroustrup/Chapter 3 - A tour of Standard Library/3.8.4 traversals and predicates/main.cpp
@@ -2,21 +2,28 @@
 #include<map>
 #include<iterator>
 #include<algorithm>
+#include<fstream>
 
 std::map<std::string,int> histogram;
 
 void record(const std::string&s){
-    histogram[s];
+    histogram[s]++;
 }
 
 void print(const std::pair<const std::string,int> &r){
     std::cout << r.first <<" " << r.second << std::endl;
 }
 
+bool get_1(std::pair<const std::string,int> &r){
+    return r.second>1;
+}
+
 int main(){
-    std::istream_iterator<std::string> ii(std::cin);
+    std::ifstream ff("test.txt");
+    std::istream_iterator<std::string> ii(ff);
     std::istream_iterator<std::string> eos;
 
     for_each(ii,eos,record);
     for_each(histogram.begin(),histogram.end(),print);
+
 }
